e73296117ff8bebbd77c4834f2f667cea6ee46a8
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-09-04 20:56:31 +0300

    Update RegisterForm.tsx

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useAuth } from '../contexts/AuthContext'; // useAuth hook'unu ekliyoruz
 import { useNavigate } from 'react-router-dom';
 
 interface RegisterFormProps {
@@ -10,10 +9,26 @@ interface RegisterFormProps {
 const RegisterForm: React.FC<RegisterFormProps> = ({ error }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [localError, setLocalError] = useState(error || ''); // localError state'i ekledik
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Sayfa yüklendiğinde hataları sıfırlıyoruz
+    setPasswordError('');
+    setLocalError('');
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    // Şifre uzunluğu kontrolü
+    if (password.length < 8) {
+      setPasswordError('Password must be at least 8 characters long.');
+      return; // Şifre hatası varsa formu gönderme
+    } else {
+      setPasswordError('');
+    }
 
     try {
       const response = await axios.post(`${process.env.REACT_APP_AUTH_ADDRESS}/api/register`, {
@@ -23,14 +38,13 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ error }) => {
 
       if (response.data.success) {
         // Başarılı kayıt işlemi, kullanıcıyı giriş sayfasına yönlendir
-        window.location.href = '/login';
+        navigate('/login');
       } else {
         // Hata durumunda mesajı göster
-        navigate('/register', { state: { error: 'Registration failed. Please try again.' } });
+        setLocalError(response.data.message);
       }
-    } catch (error:any) {
-      console.error('There was an error!', error);
-      navigate('/register', { state: { error: 'Registration failed. Please try again.' } });
+    } catch (error: any) {
+      setLocalError(error.response?.data?.message || 'An unexpected error occurred.');
     }
   };
 
@@ -63,14 +77,16 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ error }) => {
                 onChange={(e) => setPassword(e.target.value)}
                 required
                 autoComplete='current-password'
+                minLength={8}
               />
+              {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
             </div>
-            {error && <p>{error}</p>}
+            {localError && <p style={{ color: 'red' }}>{localError}</p>} {/* Hata mesajı */}
             <div className="d-flex flex-row flex-wrap justify-content-between mt-2">
               <button type="submit" className="btn btn-warning">Register</button>
               <div className="d-flex g-2 justify-content-md-between flex-column flex-md-row" style={{ gap: '4px' }}>
                 <h6 className="mt-3 pe-1 align-items-center josefin-sans-1 ps-2">Do you have an account?</h6>
-                <a href="/login" className="btn btn-outline-warning">Log in Here</a>
+                <a href="/login" className="btn btn-outline-warning align-self-center">Log in Here</a>
               </div>
             </div>
           </form>
