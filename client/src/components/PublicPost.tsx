14f043a1a465bc6465c40a8e2170af93d1c9a745
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-09-16 21:07:36 +0300

    frontend: public posts fe update

--- client/src/components/PublicPost.tsx
+++ client/src/components/PublicPost.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import '../styles/PostStyles.css'
 interface Post {
   id: number;
   title: string;
@@ -18,6 +18,8 @@ interface PublicPostProps {
   post: Post;
 }
 
+
+
 const PublicPost: React.FC<PublicPostProps> = ({ post }) => {
   const [isImageExpanded, setIsImageExpanded] = useState(false); // Fotoğrafın büyüme durumu
   const roundedRating = Math.round(post.rating);
@@ -31,77 +33,84 @@ const PublicPost: React.FC<PublicPostProps> = ({ post }) => {
     navigate(`/user/${post.user_id}`); // Profil sayfasına yönlendirme
   };
 
+  const handleBookClick = (bookId: number) => {
+    navigate(`/books/${bookId}`); // Kitap sayfasına yönlendirme
+  };
+
   return (
-    <div className="card mb-3" style={{ maxWidth: '1000px', margin: 'auto', border: '1px solid #dee2e6' }}>
-      <div className="row g-0">
-        <div className="col-md-3 d-flex justify-content-center align-items-center p-2">
-          <img
-            className={`img-fluid rounded-3 ${isImageExpanded ? 'expanded' : ''}`}
-            style={{
-              maxWidth: isImageExpanded ? '300px' : '180px',
-              maxHeight: isImageExpanded ? '300px' : '180px',
-              cursor: 'pointer',
-              transition: 'all 0.3s ease-in-out',
-            }}
-            onClick={handleImageClick}
-            src={`https://covers.openlibrary.org/b/id/${post.cover_id}.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png`}
-            alt={post.title}
-          />
-        </div>
-        <div className="col-md-9">
-          <div className="card-body">
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex flex-column">
-                <div className="d-flex align-items-center mb-2">
-                  <div
-                    className="rounded-circle d-flex justify-content-center align-items-center"
-                    onClick={post.name ? handleUserProfileClick : undefined}
-                    style={{
-                      width: '40px',
-                      height: '40px',
-                      fontSize: '20px',
-                      backgroundColor: !post.name || post.name === 'Anonym' ? '#ffffff' : '#000000',
-                      color: !post.name || post.name === 'Anonym' ? '#000000' : '#ffffff',
-                      border: !post.name || post.name === 'Anonym' ? '1px solid #000000' : '1px solid #ffffff',
-                      cursor: post.name ? 'pointer' : 'default'
-                    }}
-                  >
-                    {post.name ? post.name.charAt(0).toUpperCase() : '-'}
-                  </div>
-                  {/* Kullanıcı Bilgisi, tıklanabilir olacak */}
-                  <div className="ms-2">
-                    <h6
-                      className="card-title mb-1"
-                      onClick={post.name ? handleUserProfileClick : undefined} // Eğer post.name yoksa onClick'i devre dışı bırak
-                      style={{ cursor: post.name ? 'pointer' : 'default' }} // Cursor işaretçisi tıklanabilirlik durumuna göre değişir
+    <div className='post'> {/* Just for class filtering */}
+      <div className="card mb-3" style={{ maxWidth: '1000px', margin: 'auto', border: '1px solid #dee2e6' }}>
+        <div className="row g-0">
+          <div className="col-md-3 d-flex justify-content-center align-items-center p-2">
+            <img
+              className={`img-fluid rounded-3 ${isImageExpanded ? 'expanded' : ''}`}
+              style={{
+                maxWidth: isImageExpanded ? '300px' : '180px',
+                maxHeight: isImageExpanded ? '300px' : '180px',
+                cursor: 'pointer',
+                transition: 'all 0.3s ease-in-out',
+              }}
+              onClick={handleImageClick}
+              src={`https://covers.openlibrary.org/b/id/${post.cover_id}.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png`}
+              alt={post.title}
+            />
+          </div>
+          <div className="col-md-9">
+            <div className="card-body">
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex flex-column">
+                  <div className="d-flex align-items-center mb-2">
+                    <div
+                      className="user-profile-img rounded-circle d-flex justify-content-center align-items-center "
+                      onClick={post.name ? handleUserProfileClick : undefined}
+                      style={{
+                        backgroundColor: !post.name || post.name === 'Anonym' ? '#ffffff' : '#000000',
+                        color: !post.name || post.name === 'Anonym' ? '#000000' : '#ffffff',
+                        border: !post.name || post.name === 'Anonym' ? '1px solid #000000' : '1px solid #ffffff',
+                        cursor: post.name ? 'pointer' : 'default'
+                      }}
                     >
-                      {post.name ? post.name : 'Anonym'}
-                    </h6>
-                    <span className="text-black fw-bold">{post.title}</span>
-                    <span className="text-muted">
-                      {' '}
-                      by <span className="fst-italic"> {post.author} </span>
-                    </span>
+                      {post.name ? post.name.charAt(0).toUpperCase() : '-'}
+                    </div>
+                    {/* Kullanıcı Bilgisi, tıklanabilir olacak */}
+                    <div className="ms-2">
+                      <h6
+                        className="card-title mb-1 user-name-text"
+                        onClick={post.name ? handleUserProfileClick : undefined} // Eğer post.name yoksa onClick'i devre dışı bırak
+                        style={{ cursor: post.name ? 'pointer' : 'default' }} // Cursor işaretçisi tıklanabilirlik durumuna göre değişir
+                      >
+                        {post.name ? post.name : 'Anonym'}
+                      </h6>
+                      <span
+                        className="book-text fw-bold"
+                        style={{cursor: 'pointer'}}
+                        onClick={() => handleBookClick(post.id)}>
+                        {post.title}
+                      </span>
+                      <span className="text-muted">
+                        {' '}
+                        by <span className="fst-italic"> {post.author} </span>
+                      </span>
+                    </div>
                   </div>
                 </div>
+                <div className="text-muted" style={{ fontSize: '14px' }}>
+                  {post.is_public ? 'Public' : 'Private'}
+                </div>
               </div>
-
-              <div className="text-muted" style={{ fontSize: '14px' }}>
-                {post.is_public ? 'Public' : 'Private'}
+              <p className="card-text mt-2 overflow-auto" style={{ maxHeight: '200px' }}>
+                {post.review}
+              </p>
+              <div className="d-flex align-items-center">
+                <div className="text-warning" style={{ fontSize: '16px' }}>
+                  {Array.from({ length: roundedRating }, (_, i) => (
+                    <span key={i}>⭐</span>
+                  ))}
+                </div>
+                <span className="ms-2 text-muted">{post.rating}/5</span>
               </div>
+              <small className="text-muted mt-3">{new Date(post.time).toLocaleDateString()}</small>
             </div>
-            <p className="card-text mt-2 overflow-auto" style={{ maxHeight: '200px' }}>
-              {post.review}
-            </p>
-            <div className="d-flex align-items-center">
-              <div className="text-warning" style={{ fontSize: '16px' }}>
-                {Array.from({ length: roundedRating }, (_, i) => (
-                  <span key={i}>⭐</span>
-                ))}
-              </div>
-              <span className="ms-2 text-muted">{post.rating}/5</span>
-            </div>
-            <small className="text-muted mt-3">{new Date(post.time).toLocaleDateString()}</small>
           </div>
         </div>
       </div>
